09af656e290c47777ece59e49a41e829188138ec
KostiaDzen <k.pirozhenko95@gmail.com>
2022-04-21 12:19:53 +0300

    Slider

--- js/main.js
+++ js/main.js
@@ -44,3 +44,48 @@ function ibg(){
 }
 
 ibg();
+
+// <COPY_LINK> =========================
+const copy = document.querySelector('.copy');
+const copyLink = document.querySelector('.contract__column-container');
+const copyBlock = document.querySelector('.copy__block');
+
+copy.addEventListener('click', clickCopy);
+
+function clickCopy() {
+    navigator.clipboard.writeText(copyLink.innerText);
+    copyBlock.classList.toggle('active');
+    if (!copyBlock.classList.contains('active')) {
+        window.clearTimeout(copyBlock);
+        copyBlock.style.display = 'flex';
+        copyBlock.style.transition = 'opacity 0.5s ease-in-out';
+    } else {
+        //copyBlock.style.transition = 'opacity 1s ease-in 0s';
+        copyBlock.style.transition = ' none';
+        setTimeout(clickCopy, 500);
+    }
+}
+
+// SLIDER
+const swiper = new Swiper('.swiper', {
+    //new class
+    slidesPerView: 3,
+    effect: 'coverflow',
+    grabCursor: true,
+    slidesPerGroup: 1,
+    centeredSlides: true,
+    slidesPerColumn: 1,
+    coverflowEffect: {
+         rotate: 0,
+        //stretch: 100,
+        //depth: 1000,
+        //modifier: 10,
+        //slideShadows: true,
+    },
+    loop: true,
+    // arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
